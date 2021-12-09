2c6e8229616c90e87738db3fcbd11716e236ae74
Christopher Marchant <cmarch240@gmail.com>
2021-12-09 13:29:11 -0600

    Linted file with rubocop to fix style issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
   n = array.length
-  while true
+  loop do
     exchange_occurred = false
-    for i in 1..(n - 1)
+    (1..(n - 1)).each do |i|
       if array[i - 1] > array[i]
         array[i - 1], array[i] = array[i], array[i - 1]
         exchange_occurred = true
       end
     end
-    n = n - 1
-    break if not exchange_occurred
+    n -= 1
+    break unless exchange_occurred
   end
   array
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
 p bubble_sort([1, 3, 7, 22, -2, -5, 4, 101, 3, 4, 7, 6, 5, 1000, 953])
